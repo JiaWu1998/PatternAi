adac46f35d54c6a5f70aec774a93fb50beb91d3f
dnov09 <24783200+dnov09@users.noreply.github.com>
2018-08-01 15:38:04 -0500

    -add p5 libraries - put correct path for newly added filed

---
+++ Simple Pattern AI/BrainVisualizer/visualizer.js
@@ -0,0 +1,14 @@
+/* TODO:
+    1. Retrieve node value from html from 'id'.
+    2. Use node value to chage opacity of lines connecting to the neurons.
+*/
+
+function setup() {
+    // put setup code here
+    createCanvas(200, 200);
+}
+
+function draw() {
+    // put drawing code here
+    rect(100, 100, 100, 100);
+}
